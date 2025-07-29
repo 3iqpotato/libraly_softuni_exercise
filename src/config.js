78b22407ad3146378d8d61df921f0482376b54d4
3iqpotato <kaloianralchev@gmail.com>
2025-07-29 19:41:55 +0300

    try_1 for port switch to 3030

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const settings = {
     host: window.location.origin.includes('localhost')
-        ? 'http://localhost:3000' // Бекенд порт при локално развитие
+        ? 'http://localhost:3030' // Бекенд порт при локално развитие
         : window.location.origin // Автоматично използва текущия origin в production
 };
